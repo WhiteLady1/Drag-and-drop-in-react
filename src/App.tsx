732331023707a15f5b1c665a07a989d323bc11e0
Blanka Semanová <64536993+WhiteLady1@users.noreply.github.com>
2023-06-06 11:28:38 +0200

    Alignment (#2)
    * fix aligment on experiment page
    
    #2 Finish alignment for tablet

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Button } from './components';
 import GitHubIcon from './assets/github.svg';
 import './App.scss';
 
@@ -8,8 +8,8 @@ function App() {
     <div className="app">
       <h1 className='app__title'>Drag and drop demo</h1>
       <div className='app__wrapper'>
-        <Link className='app__wrapper__button' to='/puzzle'>Puzzle</Link>
-        <Link className='app__wrapper__button' to='/experiment'>Chemical experiment</Link>
+        <Button text='Puzzle' linkTo='/puzzle' />
+        <Button text='Chemical experiment' linkTo='/experiment' />
       </div>
       <a className='app__link' href='https://github.com/WhiteLady1/Drag-and-drop-in-react' target='_blank' rel='noopener noreferrer'>
         <img src={GitHubIcon} alt='GitHub Icon' />
---
+++ src/components/export * from './bindex.ts
@@ -0,0 +1,15 @@
+export * from '../button';
+export * from '../difficulty';
+export * from '../experimental-table';
+export * from '../message';
+export * from '../modal-img';
+export * from '../puzzle-piece';
+export * from '../result-card';
+export * from '../sample';
+export * from '../select-samples';
+export * from '../user-card';
+export * from '../user-result';
+
+export * from '../mobile-experiment';
+export * from '../tablet-experiment';
+export * from '../desktop-experiment';
