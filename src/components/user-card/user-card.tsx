24c1a97be3e2dffcf3613ae968cbf57c07cdf338
Blanka Semanova <bsemanova@deloittece.com>
2023-06-10 17:37:32 +0200

    fix build error

--- src/components/user-card/user-card.tsx
+++ src/components/user-card/user-card.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './user-card.scss';
-import { UserResult } from '../user-result';
 
 export const UserCard = () => {
   return (
     <div className='user-card'>
       <h3 className='user-card__title'>Name</h3>
-      <UserResult />
-      <UserResult />
-      <UserResult />
+      <div>lives</div>
+      <div>puzzles:
+        <p>easy</p>
+        <p>midle</p>
+        <p>hard</p>
+      </div>
     </div>
   )
 };
